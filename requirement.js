3728e5715b5abeaf44fe26e57982a5462da3673b
Troop 66 Webmaster <16478845+troop66matawan@users.noreply.github.com>
2020-04-24 18:38:02 -0400

    add approved/completed.

--- requirement.js
+++ requirement.js
@@ -1,4 +1,5 @@
 "use strict";
+const ScoutbookAuditMark = require('./auditMark');
 
 class ScoutbookRequirement {
     constructor(id,text) {
@@ -23,6 +24,31 @@ class ScoutbookRequirement {
         return this._completionDate;
     }
 
+    get markedCompleted() {
+        return this._markedCompleted;
+    }
+    set markedCompleted(value) {
+        if (value instanceof  ScoutbookAuditMark) {
+            this._markedCompleted = value;
+        }
+    }
+
+    get isApproved() {
+        return this._isApproved;
+    }
+    set isApproved(value) {
+        if (value === true || value === false) {
+            this._isApproved = value;
+        }
+    }
+    get markedApproved() {
+        return this._markedApproved;
+    }
+    set markedApproved(value) {
+        if (value instanceof  ScoutbookAuditMark) {
+            this._markedApproved = value;
+        }
+    }
 
     set requirementText(value) {
         this._requirementText = value !== undefined ? value : '';
