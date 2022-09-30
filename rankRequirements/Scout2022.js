a0acb5b297cbc93b777923be7335d0c073a85698
troop66matawan <16478845+troop66matawan@users.noreply.github.com>
2022-09-30 12:35:46 -0400

    v0.4.2 fix scout 2022 requirements

--- rankRequirements/Scout2022.js
+++ rankRequirements/Scout2022.js
@@ -10,7 +10,7 @@ class ScoutRank2022 extends ScoutbookRankRequirement {
 
     static buildRequirements() {
         const requirements = ScoutRank2016.buildRequirements();
-        requirements['1a'] = new ScoutbookRequirement('1a', 'Repeat from memory the Scout Oath, Scout Law, Scout motto, and Scout slogan. In your own words, explain their meaning. ');
+        requirements['1e'] = new ScoutbookRequirement('1e', 'Repeat from memory the Outdoor Code. List the seven principles of Leave No Trace. Explain the difference between the two.');
         requirements['5'] = new ScoutbookRequirement('5', 'Tell what you need to know about using a pocketknife safely and responsibly.');
         requirements['6'] = new ScoutbookRequirement('6', 'With your parent or guardian, complete the exercises in the pamphlet How to Protect Your Children From Child Abuse: A Parent’s Guide and earn the Cyber Chip Award for your grade  or view the Personal Safety Awareness videos ( with your parent or Guardian\'s permission)');
         return requirements;
